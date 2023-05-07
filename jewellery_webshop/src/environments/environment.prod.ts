236451c4e2264ca8ce8611e007d34e73a5353e41
Matthew2028 <72547588+Matthew2028@users.noreply.github.com>
2023-05-07 10:48:57 +0200

    deploy

--- jewellery_webshop/src/environments/environment.prod.ts
+++ jewellery_webshop/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase:{
+    apiKey: "AIzaSyAd1lqRuPtae0xQXqmmNSgwsAToP-Jyg9k",
+    authDomain: "jewellery-shop-40e97.firebaseapp.com",
+    projectId: "jewellery-shop-40e97",
+    storageBucket: "jewellery-shop-40e97.appspot.com",
+    messagingSenderId: "1084248173448",
+    appId: "1:1084248173448:web:e80c5a4b6ee6c79c0ba83d",
+    measurementId: "G-KW2DBKW15Q"
+  },
+production: true
 };
